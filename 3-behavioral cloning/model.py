743e9542fd0b29dd020071ecd3acc095b7458831
ScHarKsen <t.dorau@gmx.de>
2017-06-25 21:17:16 +0200

    Basic model cropping the image and augmenting data

--- 3-behavioral cloning/model.py
+++ 3-behavioral cloning/model.py
@@ -22,17 +22,44 @@ for line in lines:
     measurment = float(line[3])
     measurments.append(measurment)
 
-X_train = np.array(images)
-y_train = np.array(measurments)
+augmented_images, augmented_measurements = [], []
+for image, measurment in zip(images, measurments):
+    augmented_images.append(image)
+    augmented_measurements.append(measurment)
+    augmented_images.append(cv2.flip(image, 1))
+    augmented_measurements.append(measurment*-1.0)
+
+X_train = np.array(augmented_images)
+y_train = np.array(augmented_measurements)
 
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda
-from keras.layers import Convolution2D
+from keras.layers import Convolution2D, MaxPooling2D
+from keras.layers import Dropout, Cropping2D
+
 
 model = Sequential()
-model.add(Lambda(lambda x: (x / 255) - 0.5, input_shape=(160,320,3)))
+model.add(Lambda(lambda  x: x / 255.0 - 0.5, input_shape=(160,320,3)))
+model.add(Cropping2D(cropping=((70,25),(0,0))))
 model.add(Flatten())
 model.add(Dense(1))
+#nvida model
+# model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160, 320, 3)))
+# model.add(Cropping2D(cropping=((70,25), (0,0))))
+# model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation='relu'))
+# model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation='relu'))
+# model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation='relu'))
+# model.add(Convolution2D(64, 3, 3, activation='relu'))
+# model.add(Convolution2D(64, 3, 3, activation='relu'))
+# model.add(Dropout(0.25))
+# model.add(Flatten())
+# model.add(Dense(100, activation='relu'))
+# model.add(Dropout(0.25))
+# model.add(Dense(50, activation='relu'))
+# model.add(Dropout(0.25))
+# model.add(Dense(10, activation='relu'))
+# model.add(Dropout(0.25))
+# model.add(Dense(1, activation='tanh'))
 
 model.compile(loss='mse', optimizer='adam')
 model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=1)
