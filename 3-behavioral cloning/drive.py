138b31f3e42ae8c5a0774a8265fd8902c52642e4
ScHarKsen <t.dorau@gmx.de>
2017-06-26 19:18:51 +0200

    Added Umodel.py for using the data provided by udacity and using the nvdia model

--- 3-behavioral cloning/drive.py
+++ 3-behavioral cloning/drive.py
@@ -3,6 +3,7 @@ import base64
 from datetime import datetime
 import os
 import shutil
+import cv2
 
 import numpy as np
 import socketio
@@ -47,6 +48,12 @@ controller = SimplePIController(0.1, 0.002)
 set_speed = 9
 controller.set_desired(set_speed)
 
+# def prep_image(image):
+#     image = image[55:135, :, :]
+#     image = cv2.resize(image, (64, 64))
+#     image = image.astype(np.float32)
+#     image = image / 255.0 - 0.5
+#     return image
 
 @sio.on('telemetry')
 def telemetry(sid, data):
@@ -61,6 +68,10 @@ def telemetry(sid, data):
         imgString = data["image"]
         image = Image.open(BytesIO(base64.b64decode(imgString)))
         image_array = np.asarray(image)
+
+        #Preprocessing the image
+        # image_array = prep_image(image_array)
+
         steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
 
         throttle = controller.update(float(speed))
--- 3-behavioral cloning/model.py
+++ 3-behavioral cloning/model.py
@@ -3,31 +3,29 @@ import os
 import cv2
 import numpy as np
 
-
 lines = []
-with open(os.path.join("Simulationdata", "driving_log.csv")) as csvfile:
-#with open(os.path.join("data", "driving_log.csv")) as csvfile:
+with open(os.path.join("Testdata", "driving_log.csv")) as csvfile:
     reader = csv.reader(csvfile)
     for line in reader:
         lines.append(line)
 
 images = []
-measurments = []
+measurements = []
 for line in lines:
     source_path = line[0]
     filename = source_path.split('\\')[-1]
-    current_path = os.path.join("Simulationdata", "IMG", filename)
+    current_path = os.path.join("Testdata", "IMG", filename)
     image = cv2.imread(current_path)
     images.append(image)
-    measurment = float(line[3])
-    measurments.append(measurment)
+    measurement = float(line[3])
+    measurements.append(measurement)
 
 augmented_images, augmented_measurements = [], []
-for image, measurment in zip(images, measurments):
+for image, measurement in zip(images, measurements):
     augmented_images.append(image)
-    augmented_measurements.append(measurment)
+    augmented_measurements.append(measurement)
     augmented_images.append(cv2.flip(image, 1))
-    augmented_measurements.append(measurment*-1.0)
+    augmented_measurements.append(measurement*-1.0)
 
 X_train = np.array(augmented_images)
 y_train = np.array(augmented_measurements)
@@ -41,28 +39,19 @@ from keras.layers import Dropout, Cropping2D
 model = Sequential()
 model.add(Lambda(lambda  x: x / 255.0 - 0.5, input_shape=(160,320,3)))
 model.add(Cropping2D(cropping=((70,25),(0,0))))
+# model.add(Convolution2D(24,5,5,subsample=(2,2), activation='relu'))
+# model.add(Convolution2D(36,5,5,subsample=(2,2), activation='relu'))
+# model.add(Convolution2D(48,5,5,subsample=(2,2), activation='relu'))
+# model.add(Convolution2D(64,3,3,activation='relu'))
+# model.add(Convolution2D(64,3,3,activation='relu'))
 model.add(Flatten())
+#model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
 model.add(Dense(1))
-#nvida model
-# model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160, 320, 3)))
-# model.add(Cropping2D(cropping=((70,25), (0,0))))
-# model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation='relu'))
-# model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation='relu'))
-# model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation='relu'))
-# model.add(Convolution2D(64, 3, 3, activation='relu'))
-# model.add(Convolution2D(64, 3, 3, activation='relu'))
-# model.add(Dropout(0.25))
-# model.add(Flatten())
-# model.add(Dense(100, activation='relu'))
-# model.add(Dropout(0.25))
-# model.add(Dense(50, activation='relu'))
-# model.add(Dropout(0.25))
-# model.add(Dense(10, activation='relu'))
-# model.add(Dropout(0.25))
-# model.add(Dense(1, activation='tanh'))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=1)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=5)
 
 model.save('model.h5')
 
