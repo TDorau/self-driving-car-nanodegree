c7a1231979af6323141bb2919726edd16742c9c2
ScHarKsen <t.dorau@gmx.de>
2017-08-25 23:44:02 +0200

    Added class_structure

---
+++ 5-vehicle-detection-and-tracking/class_structure.py
@@ -0,0 +1,103 @@
+# Object Tracker
+
+##### Globals needed ######
+global cars
+###########################
+
+
+class Object:
+    def __init__(self,position):
+        self.position = position
+        self.new_postion = None
+        self.count = 0
+        self.frame = 1
+        self.flag = False
+        self.long_count = 0
+        self.postion_average = []
+
+    def update(self,temp_position):
+        if abs(temp_position[2]-self.position[2]) < 100 and abs(temp_position[3]-self.position[3]) < 100:
+            if self.long_count > 2:
+                self.postion_average.pop(0)
+                self.postion_average.append(temp_position)
+                self.new_postion = np.mean(np.array(self.postion_average), axis=0).astype(int)
+                self.position = self.new_postion
+                self.frame = 1
+                self.count += 1
+
+                return False
+
+            self.position = temp_position
+            self.postion_average.append(temp_position)
+            self.count+=1
+
+            return False
+
+        else:
+            return True
+
+    def get_position(self):
+        self.frame+=1
+        if self.count == 7 and self.long_count < 3 :
+            self.new_postion = np.mean(np.array(self.postion_average), axis=0).astype(int)
+            self.count = 0
+            self.frame = 1
+            self.long_count += 1
+            if self.long_count < 2:
+                self.postion_average = []
+
+        if self.frame > 10:
+            self.flag = True
+
+        return self.new_postion, self.flag
+
+class Vehicle(Object):
+     def __init__(self, position):
+        Object.__init__(self, position)
+
+class Person(Object):
+    def __init__(self, position):
+        Object.__init__(self, position)
+
+class Sign(Object):
+    def __init__(self, position):
+        Object.__init__(self, position)
+        self.label = None
+        run_network()
+    def run_network(self):
+        # Crop out position and run through sign network
+        # update self.label
+
+
+
+
+# Takes in a list of calculated centroids calculated from current frame from your own code  (both good and bad)
+
+# Deal with car tracking
+for centroid in img_centroids:
+    new = True
+    for car in cars:
+        new = car.update(centroid)
+        if new == False:
+            break
+    if new == True:
+        cars.append(Vehicle(centroid))
+
+next_cars = []
+positions = []
+
+for car in cars:
+    position, flag = car.get_position()
+    if flag == False:
+        next_cars.append(car)
+    positions.append(position)
+
+cars = next_cars
+
+# Outputs current relevant positions.
+
+try:
+    for (x1, y1, x2, y2) in positions:
+        cv2.rectangle(clone, (x1, y1), (x2, y2), (255, 0, 0), thickness=2)
+except:
+    pass
