6c3ca77dad487903e532ac0c505b1b0b4621451b
ScHarKsen <t.dorau@gmx.de>
2017-11-12 00:43:24 +0100

    Fixed error in constraints, Added penalty term

--- 10-model-predictive-control/src/MPC.cpp
+++ 10-model-predictive-control/src/MPC.cpp
@@ -8,7 +8,7 @@ using CppAD::AD;
 // TODO: Set the timestep length and duration 
 // I also tried an N of 20 -> more likely to fall of the cliff
 // 10 * 0.1 goes 1s into the future
-size_t N = 12;
+size_t N = 10;
 double dt = 0.1;
 
 // This value assumes the model presented in the classroom is used.
@@ -67,6 +67,7 @@ class FG_eval {
     for (int i = 0; i < N - 1; i++) {
     	fg[0] += 5*CppAD::pow(vars[delta_start + i], 2);
     	fg[0] += 5*CppAD::pow(vars[a_start + i], 2);
+    	fg[0] += 500*CppAD::pow(vars[delta_start + i] * vars[v_start+i], 2);
     }
 
     // Minimize the value gap between sequential actuations, next control input quite similar to current one
@@ -88,37 +89,40 @@ class FG_eval {
     fg[1 + epsi_start] = vars[epsi_start];
 
     //the rest of the contraints
-    for (int i = 0; i < N - 1; i++) {
+    for (int t = 1; t < N; t++) {
     	//values at time t + 1
-    	AD<double> x1 = vars[x_start + i +1];
-    	AD<double> y1 = vars[y_start + i +1];
-    	AD<double> psi1 = vars[psi_start + i +1];
-    	AD<double> v1 = vars[v_start + i +1];
-    	AD<double> cte1 = vars[cte_start + i +1];
-    	AD<double> epsi1 = vars[epsi_start + i + 1];
-    	// values at time t
-    	AD<double> x0 = vars[x_start + i];
-    	AD<double> y0 = vars[y_start + i];
-    	AD<double> psi0 = vars[v_start + i];
-    	AD<double> v0 = vars[v_start + i];
-    	AD<double> cte0 = vars[cte_start + i];
-    	AD<double> epsi0 = vars[epsi_start + i];
+	  	// The state at time t+1 .
+	  	AD<double> x1 = vars[x_start + t];
+	  	AD<double> y1 = vars[y_start + t];
+	  	AD<double> psi1 = vars[psi_start + t];
+	  	AD<double> v1 = vars[v_start + t];
+	  	AD<double> cte1 = vars[cte_start + t];
+	  	AD<double> epsi1 = vars[epsi_start + t];
 
-    	AD<double> delta0 = vars[delta_start + i];
-    	AD<double> a0 = vars[a_start + i];
+	  	// The state at time t.
+	  	AD<double> x0 = vars[x_start + t - 1];
+	  	AD<double> y0 = vars[y_start + t - 1];
+	  	AD<double> psi0 = vars[psi_start + t - 1];
+	  	AD<double> v0 = vars[v_start + t - 1];
+	  	AD<double> cte0 = vars[cte_start + t - 1];
+	  	AD<double> epsi0 = vars[epsi_start + t - 1];
 
-    	AD<double> f0 = coeffs[0] + coeffs[1] * x0 + coeffs[2] * x0 * x0 +coeffs[3] * x0 * x0 *x0;
+	 	 // Only consider the actuation at time t.
+	  	AD<double> delta0 = vars[delta_start + t - 1];
+	  	AD<double> a0 = vars[a_start + t - 1];
+
+    	AD<double> f0 = coeffs[0] + coeffs[1] * x0 + coeffs[2] * CppAD::pow(x0, 2) + coeffs[3] * CppAD::pow(x0, 3);
     	AD<double> psides0 = CppAD::atan(3*coeffs[3]*x0*x0 + 2*coeffs[2] * x0 + coeffs[1]);
 
     	//Here is x to get you started
     	//The idea here is to constraint this value to 0
     	// These are the model equations from the classromm
-    	fg[2 + x_start + i] = x1 - (x0 + v0 * CppAD::cos(psi0) * dt);
-    	fg[2 + y_start + i] = y1 - (y0 + v0 * CppAD::sin(psi0) * dt);
-    	fg[2 + psi_start + i] = psi1 - (psi0 - v0 * delta0 / Lf * dt);
-    	fg[2 + v_start + i] = v1 - (v0 + a0 * dt);
-    	fg[2 + cte_start + i] = cte1 -((f0 - y0) + (v0 * CppAD::sin(epsi0) * dt));
-    	fg[2 + epsi_start + i] = epsi1 - ((psi0 - psides0) - v0 * delta0 / Lf * dt);
+    	fg[1 + x_start + t] = x1 - (x0 + v0 * CppAD::cos(psi0) * dt);
+    	fg[1 + y_start + t] = y1 - (y0 + v0 * CppAD::sin(psi0) * dt);
+    	fg[1 + psi_start + t] = psi1 - (psi0 - v0 * delta0 / Lf * dt);
+    	fg[1 + v_start + t] = v1 - (v0 + a0 * dt);
+    	fg[1 + cte_start + t] = cte1 -((f0 - y0) + (v0 * CppAD::sin(epsi0) * dt));
+    	fg[1 + epsi_start + t] = epsi1 - ((psi0 - psides0) - v0 * delta0 / Lf * dt);
     }
 
   }
